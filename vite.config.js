404b40be375af3f558fc3bb9c2e93447d31cedc1
BlackBlossom <vikingpanther72@gmail.com>
2025-11-16 22:52:38 +0530

    fix: updated the ideayex backend URL

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,23 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
-    proxy: {
-      '/api/v1': {
-        target: 'http://ecellideatex.theprelofts.in:8080',
-        changeOrigin: true,
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('Proxy error:', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response:', proxyRes.statusCode, req.url);
-          });
-        }
-      }
+    // proxy: {
+    //   '/api/v1': {
+    //     target: 'http://ecellideatex.theprelofts.in:8080',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     configure: (proxy, _options) => {
+    //       proxy.on('error', (err, _req, _res) => {
+    //         console.log('Proxy error:', err);
+    //       });
+    //       proxy.on('proxyReq', (proxyReq, req, _res) => {
+    //         console.log('Sending Request to:', req.method, req.url);
+    //       });
+    //       proxy.on('proxyRes', (proxyRes, req, _res) => {
+    //         console.log('Received Response:', proxyRes.statusCode, req.url);
+    //       });
+    //     }
+    //   }
     }
   }
 })
